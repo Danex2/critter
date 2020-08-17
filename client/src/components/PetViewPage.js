3f123ba56651892b928a705396263bdf6a53af75
Danex2 <khadane.miller@gmail.com>
2020-08-16 23:36:07 -0400

    switch to parcel

--- client/src/components/PetViewPage.js
+++ client/src/components/PetViewPage.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import axios from 'axios';
-import PetViewItem from './PetViewItem';
+import React from "react";
+import axios from "axios";
+import PetViewItem from "./PetViewItem";
 
 const PetsView = () => {
   React.useEffect(() => {
-    document.title = 'Find My Pet - Search';
+    document.title = "Find My Pet - Search";
   }, []);
   const [pets, setPets] = React.useState([]);
   React.useEffect(() => {
-    axios.get('/pets').then(pets => setPets(pets.data.pets));
+    axios.get("/pets").then((pets) => setPets(pets.data.pets));
   }, []);
   return (
-    <div className='flex flex-wrap p-4 flex justify-center'>
-      {!pets.length ? (
+    <div className="flex flex-wrap p-4 flex justify-center">
+      {!pets?.length ? (
         <p>No active pet ads.</p>
       ) : (
-        pets.map(pet => (
+        pets?.map((pet) => (
           <PetViewItem
             key={pet._id}
             id={pet._id}
